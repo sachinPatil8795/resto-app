7a9d3cac903d5f0e64c4fcc0d440f81fc0ab03f2
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-30 20:45:05 +0530

    header completed

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import mealsImage from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>FriendsCorner</h1>
-        <button>cart</button>
+        <HeaderCartButton />
       </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt='Delicious food!' />
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Delicious food!" />
       </div>
     </>
   );
