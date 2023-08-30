cd1fcc305db6a09363c5001847e78777473b76c5
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-30 21:10:36 +0530

    Meals summary added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <MealsSummary />
     </div>
   );
 };
