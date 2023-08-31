e62d7253e6289097f50fb37b0b9e8298612b0350
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-31 13:34:47 +0530

    Added form input and button

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-function MealItemForm() {
+const MealItemForm = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
