a2c3ec7a89fb4e53eec2b7f637551eb3907780ab
sachin_sp95 <sachinsp8795@gmail.com>
2023-08-31 17:02:51 +0530

    Modal functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
+const CartContext = React.createContext();
+
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <CartContext.Provider value={showCartHandler}>
+        <Header />
+      </CartContext.Provider>
       <main>
         <Meals />
       </main>
@@ -16,3 +30,4 @@ const App = () => {
 };
 
 export default App;
+export {CartContext};
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import { CartContext } from "../../App";
 
 const HeaderCartButton = (props) => {
+  const contextCart = useContext(CartContext);
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={contextCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
